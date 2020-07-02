b6a595beec99bfcc06c02b64b41f2409af788c3a
AgZaw <mgaungzawios@gmail.com>
2020-07-02 21:54:56 +0630

    Updates

---
+++ precache-manifest.48eb8b9f0b8bbe455bfb4505ebeef819.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bd55c5434939fff4682911e8d5b3d2b",
+    "url": "/mmrexam/index.html"
+  },
+  {
+    "revision": "a3a0868363af710d89e0",
+    "url": "/mmrexam/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "005f8de7c563456d4aa0",
+    "url": "/mmrexam/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "a3a0868363af710d89e0",
+    "url": "/mmrexam/static/js/2.c2ded1c7.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/mmrexam/static/js/2.c2ded1c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "005f8de7c563456d4aa0",
+    "url": "/mmrexam/static/js/main.04bfa320.chunk.js"
+  },
+  {
+    "revision": "6cfbf7a6b6c0cdf6530c",
+    "url": "/mmrexam/static/js/runtime-main.9eb18047.js"
+  }
+]);
