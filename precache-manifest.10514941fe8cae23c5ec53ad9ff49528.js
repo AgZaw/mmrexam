b8184734c54ebaac19044c3535b9603808eba1ff
AgZaw <mgaungzawios@gmail.com>
2020-07-02 23:18:56 +0630

    Updates

---
+++ precache-manifest.10514941fe8cae23c5ec53ad9ff49528.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79dc8fe7f07d1201d93dae2c89dc5b3e",
+    "url": "/mmrexam/index.html"
+  },
+  {
+    "revision": "63d9cdbcc7bca73526ed",
+    "url": "/mmrexam/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "82f88101e0aa86a58999",
+    "url": "/mmrexam/static/css/main.463b38f9.chunk.css"
+  },
+  {
+    "revision": "63d9cdbcc7bca73526ed",
+    "url": "/mmrexam/static/js/2.0adea9bb.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/mmrexam/static/js/2.0adea9bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82f88101e0aa86a58999",
+    "url": "/mmrexam/static/js/main.6099b05f.chunk.js"
+  },
+  {
+    "revision": "6cfbf7a6b6c0cdf6530c",
+    "url": "/mmrexam/static/js/runtime-main.9eb18047.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mmrexam/precache-manifest.6aaa5883e2cafc6a095de126352d8ecd.js"
+  "/mmrexam/precache-manifest.10514941fe8cae23c5ec53ad9ff49528.js"
 );
 
 self.addEventListener('message', (event) => {
